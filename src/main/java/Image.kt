2d19dd092a355aeb0d4a07cabfd9f673d2003f43
Latiif <llusx@outlook.com>
2018-07-29 12:46:07 +0200

    Add export functionality to class Image
    Improve  setter and getters

--- src/main/java/Image.kt
+++ src/main/java/Image.kt
@@ -3,13 +3,13 @@ import java.io.File
 import java.io.FileNotFoundException
 import javax.imageio.ImageIO
 
-class Image(val imgPath:String){
+class Image(val imgPath: String) {
 
-    val image:BufferedImage
-    val height:Int
-    val width:Int
+    val image: BufferedImage
+    val height: Int
+    val width: Int
 
-    init{
+    init {
         val imageFile = File(imgPath)
 
         if (!imageFile.exists()) throw FileNotFoundException("File $imgPath not found.")
@@ -22,73 +22,78 @@ class Image(val imgPath:String){
     }
 
 
-
-
-    operator fun get(i:Int,j:Int):Pixel{
-        require (i in 0..width && j in 0..height)
-        return Pixel(image.getRGB(i,j))
-    }
-
-    operator fun set(i:Int,j:Int,p:Pixel):Unit{
+    operator fun get(i: Int, j: Int): Pixel {
         require(i in 0..width && j in 0..height)
-        image.setRGB(i,j,p.getColorCode())
+        return Pixel(image.getRGB(i, j))
     }
 
+    operator fun set(i: Int, j: Int, p: Pixel): Unit {
+        require(i in 0..width && j in 0..height)
+        image.setRGB(i, j, p.getColorCode())
+    }
 
+    fun exportImage(imagePath: String = "image", imageType: String = "jpg") {
+
+
+        val outputfile = File("$imagePath.$imageType")
+        ImageIO.write(image, imageType, outputfile)
+
+    }
 
 }
 
-class Pixel(colorCode:Int){
+class Pixel(colorCode: Int) {
 
-    private var alpha:Byte
-    private var r:Byte; private var g:Byte; private var b:Byte
+    private var alpha: Byte
+    private var r: Byte;
+    private var g: Byte;
+    private var b: Byte
 
 
-    init{
+    init {
         b = (colorCode and 0xff).toByte()
         g = (colorCode and 0xff00 shr 8).toByte()
         r = (colorCode and 0xff0000 shr 16).toByte()
 
-        alpha= (colorCode and 0xff000000.toInt() ushr 32).toByte()
+        alpha = (colorCode and 0xff000000.toInt() ushr 32).toByte()
     }
 
 
-    var Alpha:Byte
+    var Alpha: Byte
         get() = alpha
-        set(value:Byte){
-            require(value in 0..255)
-            alpha=value
+        set(value: Byte) {
+            require(value.isByte())
+            alpha = value
         }
 
-    var R:Byte
+    var R: Byte
         get() = r
-        set(value:Byte){
-            require(value in 0..255)
-            r=value
+        set(value: Byte) {
+            require(value.isByte())
+            r = value
         }
 
 
-    var G:Byte
+    var G: Byte
         get() = g
-        set(value:Byte){
-            require(value in 0..255)
-            g=value
+        set(value: Byte) {
+            require(value.isByte())
+            g = value
         }
 
 
-
-    var B:Byte
+    var B: Byte
         get() = b
-        set(value:Byte){
-            require(value in 0..255)
-            b=value
+        set(value: Byte) {
+            require(value.isByte())
+            b = value
         }
 
 
-    fun getColorCode():Int {
-        val hex = String.format("%02x%02x%02x%02x",alpha, r, g, b)
+    fun Byte.isByte()= this in Byte.MIN_VALUE..Byte.MAX_VALUE
+
+    fun getColorCode(): Int {
+        val hex = String.format("%02x%02x%02x%02x", alpha, r, g, b)
         return hex.toLong(16).toInt()
     }
-
-
 }
