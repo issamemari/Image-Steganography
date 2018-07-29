c28bff407e527a2dfbee40e575f8ca88fb41be8a
Latiif <llusx@outlook.com>
2018-07-29 12:10:07 +0200

    Add Image class

---
+++ src/main/java/Image.kt
@@ -0,0 +1,94 @@
+import java.awt.image.BufferedImage
+import java.io.File
+import java.io.FileNotFoundException
+import javax.imageio.ImageIO
+
+class Image(val imgPath:String){
+
+    val image:BufferedImage
+    val height:Int
+    val width:Int
+
+    init{
+        val imageFile = File(imgPath)
+
+        if (!imageFile.exists()) throw FileNotFoundException("File $imgPath not found.")
+        if (imageFile.isDirectory) throw IllegalArgumentException("$imgPath is a directory, not an image.")
+
+        image = ImageIO.read(imageFile)
+
+        width = image.width
+        height = image.height
+    }
+
+
+
+
+    operator fun get(i:Int,j:Int):Pixel{
+        require (i in 0..width && j in 0..height)
+        return Pixel(image.getRGB(i,j))
+    }
+
+    operator fun set(i:Int,j:Int,p:Pixel):Unit{
+        require(i in 0..width && j in 0..height)
+        image.setRGB(i,j,p.getColorCode())
+    }
+
+
+
+}
+
+class Pixel(colorCode:Int){
+
+    private var alpha:Byte
+    private var r:Byte; private var g:Byte; private var b:Byte
+
+
+    init{
+        b = (colorCode and 0xff).toByte()
+        g = (colorCode and 0xff00 shr 8).toByte()
+        r = (colorCode and 0xff0000 shr 16).toByte()
+
+        alpha= (colorCode and 0xff000000.toInt() ushr 32).toByte()
+    }
+
+
+    var Alpha:Byte
+        get() = alpha
+        set(value:Byte){
+            require(value in 0..255)
+            alpha=value
+        }
+
+    var R:Byte
+        get() = r
+        set(value:Byte){
+            require(value in 0..255)
+            r=value
+        }
+
+
+    var G:Byte
+        get() = g
+        set(value:Byte){
+            require(value in 0..255)
+            g=value
+        }
+
+
+
+    var B:Byte
+        get() = b
+        set(value:Byte){
+            require(value in 0..255)
+            b=value
+        }
+
+
+    fun getColorCode():Int {
+        val hex = String.format("%02x%02x%02x%02x",alpha, r, g, b)
+        return hex.toLong(16).toInt()
+    }
+
+
+}
