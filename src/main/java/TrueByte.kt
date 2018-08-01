3c09c237fa0508cf482f305a30dc0c46168b7610
Latiif <llusx@outlook.com>
2018-08-01 17:42:35 +0200

    Implement equals, toString, hashCode and get operator

--- src/main/java/TrueByte.kt
+++ src/main/java/TrueByte.kt
@@ -5,6 +5,7 @@ class TrueByte(val value: Byte) {
         return TrueByte(and255(this.value.toInt() and trueByte.value.toInt()).toByte())
     }
 
+
     infix fun or(trueByte: TrueByte):TrueByte{
         return TrueByte(and255(this.value.toInt() or trueByte.value.toInt()).toByte())
     }
@@ -34,5 +35,29 @@ class TrueByte(val value: Byte) {
         return int and 255
     }
 
+
+    override operator fun equals(other:Any?):Boolean{
+        return when (other){
+            null -> false
+            is TrueByte -> this.value.equals(other.value)
+            is Byte -> this.value.equals(other)
+            is Int -> this.value.equals(other.toByte())
+            else -> false
+        }
+    }
+
+
+    override fun toString(): String{
+        return and255(value.toInt()).toString()
+    }
+
+    operator fun get(position: Int): Boolean {
+        require(position in 0..7)
+        return (this shr position) and TrueByte(1) == TrueByte(1)
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
 
 }
