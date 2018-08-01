2353474f4f88e08f514318d0fa221d7b2dac9203
Latiif <llusx@outlook.com>
2018-08-01 17:16:48 +0200

    Make TrueByte Immutable
    Add and, or, xor and inv operations

--- src/main/java/TrueByte.kt
+++ src/main/java/TrueByte.kt
@@ -1,7 +1,22 @@
-class TrueByte(var value: Byte) {
+class TrueByte(val value: Byte) {
 
 
-    infix fun and()
+    infix fun and(trueByte: TrueByte):TrueByte{
+        return TrueByte(and255(this.value.toInt() and trueByte.value.toInt()).toByte())
+    }
+
+    infix fun or(trueByte: TrueByte):TrueByte{
+        return TrueByte(and255(this.value.toInt() or trueByte.value.toInt()).toByte())
+    }
+
+    infix fun xor(trueByte: TrueByte):TrueByte{
+        return TrueByte(and255(this.value.toInt() xor trueByte.value.toInt()).toByte())
+    }
+
+    fun inv():TrueByte{
+        return TrueByte(and255(this.value.toInt().inv()).toByte())
+    }
+
 
     infix fun shr(bitCount: Int): TrueByte {
         if (bitCount < 0) return shr(-bitCount)
